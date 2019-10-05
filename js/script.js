0bb731690224758180e31531b7fbb8effa43da1b
Alex <9970421@gmail.com>
2019-10-05 17:13:46 +0300

    Исправлены ошибки

--- js/script.js
+++ js/script.js
@@ -7,47 +7,47 @@ const fullName = modal.querySelector('[name=message-fullname]');
 const eMail = modal.querySelector('[name=message-email]');
 const message = modal.querySelector('[name=message-text]');
 
-
-
 if (modalButton && closeButton && overlay && modal) {
     modalButton.addEventListener('click', openModal);
     closeButton.addEventListener('click', closeModal);
     overlay.addEventListener('click', closeModal);
 
-    function openModal(e) {
-        e.preventDefault();
+    function openModal(event) {
+        event.preventDefault();
 
-        overlay.classList.add('modal-open');
+        overlay.classList.add('overlay-open');
         modal.classList.add('modal-open');
         fullName.focus();
-
     }
 
-    function closeModal(e) {
-        e.preventDefault();
+    function closeModal(event) {
+        event.preventDefault();
 
-        overlay.classList.remove('modal-open');
+        overlay.classList.remove('overlay-open');
         modal.classList.remove('modal-open');
         modal.classList.remove('modal-error');
     }
 
-};
-window.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (modal.classList.contains('modal-open')) {
-            modal.classList.remove('modal-open');
-            overlay.classList.remove('modal-open');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        if (!fullName.value || !eMail.value || !message.value) {
+            modal.classList.remove('modal-error');
+            modal.offsetWidth = modal.offsetWidth;
+            modal.classList.add('modal-error');
+        } else {
+            closeModal(event);
         }
+    });
 
-    }
-});
+    window.addEventListener('keydown', function(event) {
+        if (event.keyCode === 27) {
+            event.preventDefault();
 
-form.addEventListener('submit', function(evt) {
-    if (!fullName.value || !eMail.value || !message.value) {
-        evt.preventDefault();
-        modal.classList.remove('modal-error');
-        modal.offsetWidth = modal.offsetWidth;
-        modal.classList.add('modal-error');
-    }
-});
+            if (modal.classList.contains('modal-open')) {
+                modal.classList.remove('modal-open');
+                overlay.classList.remove('overlay-open');
+            }
+        }
+    });
+};
